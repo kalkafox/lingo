df3b04ce4148c396641228d41fa4d9162d599196
Kalka <k@kalka.io>
2023-12-22 02:00:18 -0500

    add run to excludes, properly invoke loading spinner

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   host: process.env.DATABASE_HOST,
-  port: 52378,
+  port: parseInt(process.env.DATABASE_PORT as string),
   user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: 'lingo',
