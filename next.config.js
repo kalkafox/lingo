78e3aa2319f8b2c86921d4a59ef03efe70bde1f6
Kalka <k@kalka.io>
2024-01-08 10:41:35 -0500

    check if this is even the problem

--- next.config.js
+++ next.config.js
@@ -19,18 +19,4 @@ const nextConfig = {
 //   skipWaiting: true,
 // })
 
-const {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-} = require('next/constants')
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    const withSerwist = require('@serwist/next').default({
-      swSrc: 'app/sw.ts',
-      swDest: 'public/sw.js',
-    })
-    return withSerwist(nextConfig)
-  }
-  return nextConfig
-}
+module.exports = nextConfig
