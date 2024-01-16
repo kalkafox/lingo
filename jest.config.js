78e191be143241259bec43f05a7ccefbf36db2d4
Noah Overcash <noah@overca.sh>
2024-01-15 22:49:18 -0500

    behave that way, then (2)

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
+const path = require('path');
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
 
   testMatch: ['**/src/**/?(*.)test.{js,jsx,ts,tsx}'],
-  setupFilesAfterEnv: ['./src/test/jest.setup.ts'],
+  setupFilesAfterEnv: [path.join(import.meta.dir, 'src/test/jest.setup.ts')],
 
   globals: {
     "ts-jest": {
-      tsconfig: "./tsconfig.jest.json"
+      tsconfig: path.join(import.meta.dir, "tsconfig.jest.json")
     }
   }
 };
