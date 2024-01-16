852dca3aaf02f89cd8a95c68428ca7635d5c9bb1
Noah Overcash <noah@overca.sh>
2024-01-15 22:41:51 -0500

    feat: add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+
+  testMatch: ['./src/**/?(*.)test.{js,jsx,ts,tsx}'],
+  setupFilesAfterEnv: ['./src/test/jest.setup.ts'],
+
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.jest.json"
+    }
+  }
+};
