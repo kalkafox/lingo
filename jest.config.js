d9e2926449cd768a6175e67c5c8152e31e654ca9
Noah Overcash <noah@overca.sh>
2024-01-15 22:49:53 -0500

    omg

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,11 @@ module.exports = {
   testEnvironment: 'jsdom',
 
   testMatch: ['**/src/**/?(*.)test.{js,jsx,ts,tsx}'],
-  setupFilesAfterEnv: [path.join(import.meta.dir, 'src/test/jest.setup.ts')],
+  setupFilesAfterEnv: [path.join(__dirname, 'src/test/jest.setup.ts')],
 
   globals: {
     "ts-jest": {
-      tsconfig: path.join(import.meta.dir, "tsconfig.jest.json")
+      tsconfig: path.join(__dirname, "tsconfig.jest.json")
     }
   }
 };
